4a8c6e2e93049c60573a4ed1b59d63ddf77d631f
Antti Kumavaara <antti.kumavaara@tuni.fi>
2020-11-30 23:12:02 +0200

    Added some more tests for the add function

--- tests/add.test.js
+++ tests/add.test.js
@@ -1,5 +1,16 @@
-import add from '../src/add'
+import add from '../src/add.js'
 
-test('#add()', () => {
-    expect(add(2,3)).toBe(5);        
+describe("Testing the add function",() => {
+    test("Positive numbers",() => {
+        expect(add(2, 3)).toBe(5);
+    });
+
+    test("Negative numbers", () => {
+        expect(add(-15,-12)).toBe(-27);
+    });
+
+    test("Decimal numbers", () => {
+        expect(add(5.55,6.666)).toBeCloseTo(12.216,3);
+    });
+
 });
