18267682ff96fa0f1b50f383dcdb832a9f4468e7
Antti Kumavaara <antti.kumavaara@tuni.fi>
2020-12-04 00:05:38 +0200

    Added tests for words function

---
+++ tests/words.test.js
@@ -0,0 +1,10 @@
+import words from '../src/words'
+describe("Testing the words function",() => {
+    test("Extra & in text",() => {
+        expect(words('fred, bob, maverick, & jonahtan')).toStrictEqual(['fred', 'bob', 'maverick', 'jonahtan']);
+    });
+
+    test("Test on the pattern option", ()=>{
+        expect(words('fred, bob, maverick, & jonahtan', /[^, ]+/g)).toStrictEqual(['fred', 'bob', 'maverick', '&', 'jonahtan']);
+    });
+});
