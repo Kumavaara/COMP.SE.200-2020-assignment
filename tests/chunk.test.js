3f4ba39bc9c60ab0db23ac8610c7f90c4468759b
Antti Kumavaara <antti.kumavaara@tuni.fi>
2020-12-04 03:02:10 +0200

    added tests for chunk (all fail)

---
+++ tests/chunk.test.js
@@ -0,0 +1,15 @@
+import chunk from '../src/chunk.js'
+
+describe("Testing the chunk function",() => {
+    test("Split in half",() => {
+        expect(chunk(['a', 'b', 'c', 'd'], 2)).toStrictEqual([['a', 'b'], ['c', 'd']]);
+    });
+    test("Split in groups of 3",() => {
+        expect(chunk(['a', 'b', 'c', 'd'], 3)).toStrictEqual([['a', 'b', 'c'], ['d']]);
+    });
+    test("Split in groups of 1",() => {
+        expect(chunk(['a', 'b', 'c', 'd'])).toStrictEqual([['a'], ['b'], ['c'], ['d']]);
+    });
+
+
+});
